aedfaefe3a6747f3476e2c5ee2459e693026dc61
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-01-18 20:48:21 +0530

    Add files via upload

---
+++ DefaultArguments.py
@@ -0,0 +1,12 @@
+"""
+Title     : Default Arguments
+Subdomain : Debugging
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/default-arguments/problem
+"""
+
+
+def print_from_stream(n, stream=EvenStream()):
+    stream.__init__()
+    for _ in range(n):
+        print(stream.get_next())
